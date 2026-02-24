c1409a3a5ba986a80fdbf8835f3bee492de8e1bf
darknight <illuminating.me@gmail.com>
2026-02-24 17:22:35 +0800

    Add extension icons and configure manifest icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
     version: '0.2.0',
     permissions: ['storage'],
     host_permissions: ['https://x.com/*', 'https://twitter.com/*'],
+    icons: {
+      16: '/icon-16.png',
+      48: '/icon-48.png',
+      96: '/icon-96.png',
+      128: '/icon-128.png',
+    },
   },
   runner: {
     startUrls: ['https://x.com/home'],
